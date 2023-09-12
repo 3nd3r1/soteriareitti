dd61a974dab5c36e59bc342db147cc5e418b187f
3nd3r1 <ender.spam.guru@gmail.com>
2023-09-12 10:22:45 +0300

    Poistetaan .vscode

--- tasks.py
+++ tasks.py
@@ -1,10 +1,12 @@
+""" tasks.py """
+
 import sys
 from invoke.tasks import task
 
 
 @task
 def start(ctx):
-    if sys.platform == "win32" or sys.platform == "win64" or sys.platform == "win-amd64":
+    if sys.platform.startswith("win"):
         ctx.run("py soteriareitti/main.py")
     else:
         ctx.run("python soteriareitti/main.py")
@@ -14,7 +16,7 @@ def start(ctx):
 
 @task
 def debug(ctx):
-    if sys.platform == "win32" or sys.platform == "win64" or sys.platform == "win-amd64":
+    if sys.platform.startswith("win"):
         ctx.run("py soteriareitti/main.py debug")
     else:
         ctx.run("python soteriareitti/main.py debug")
